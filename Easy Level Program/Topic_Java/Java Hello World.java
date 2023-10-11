296721566f0c48795b31610107bb8cccd5b9c9c8
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-11 11:49:38 +0530

    Create Java Hello World.java

---
+++ Easy Level Program/Topic_Java/Java Hello World.java
@@ -0,0 +1,19 @@
+//Approach 1 - Just Print...
+class Solution{
+    static void printHelloWorld()
+    {
+        // code here
+        System.out.print("Hello World");
+    }
+}
+
+
+//Approach 2 - Store in Variables and Print the Variable
+class Solution{
+    static void printHelloWorld()
+    {
+        // code here
+        String str = "Hello World";
+        System.out.print(str);
+    }
+}
