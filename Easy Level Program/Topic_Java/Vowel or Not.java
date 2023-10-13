3014f906afa0f58c5de125ba4d1fee6d55a02e8c
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-13 23:51:35 +0530

    Create Vowel or Not.java

---
+++ Easy Level Program/Topic_Java/Vowel or Not.java
@@ -0,0 +1,10 @@
+class Solution{
+    String isVowel(char c){
+        // code here
+        if(c=='a'||c=='A' || c=='e'||c=='E' || c=='I'||c=='i' || c=='o'||c=='O' || c=='u'||c=='U'){
+            return "YES";
+        }
+        else
+        return "NO";
+    }
+}
