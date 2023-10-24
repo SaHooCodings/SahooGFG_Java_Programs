590bbf3b624f59d9466955f155bc6422e7103170
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-24 09:55:31 +0530

    Create Arrays (Sum of array).java

---
+++ Easy Level Program/Arrays/Arrays (Sum of array).java
@@ -0,0 +1,12 @@
+class Solution {
+    
+    public long getSum(long a[], long n)
+    {
+        int sum =0;
+        for (int i = 0;i<n;i++){
+            sum+=a[i];
+        }
+        return sum;
+    }
+    
+}
