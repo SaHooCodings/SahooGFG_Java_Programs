d1e96e877be04aa437913064dd6ad5c6375ee8f3
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 00:52:16 +0530

    Create Missing number in array.java

---
+++ Easy Level Program/Arrays-Searching/Missing number in array.java
@@ -0,0 +1,11 @@
+class Solution {
+    int missingNumber(int array[], int n) {
+        // Your Code Here
+        int total = (n+1)*(n)/2;
+        for(int i = 0;i < n-1;i++)
+        {
+            total -= array[i];
+        }
+        return total;
+    }
+}
