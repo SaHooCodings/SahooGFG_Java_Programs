cd5753dbcf85e092b38f985d39066cd30cb4b902
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-24 11:11:57 +0530

    Create Perfect Arrays.java

---
+++ Easy Level Program/Arrays/Perfect Arrays.java
@@ -0,0 +1,18 @@
+class Solution{
+    
+    public boolean IsPerfect(int a[], int n)
+    {
+        // Complete the function
+        int start = 0;
+        int end = n-1;
+        while(start<end)
+        {
+            if(a[start] != a[end])
+            return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
+}
