1638e7b38394e73363ff33f544aa9ee51fed430c
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 00:11:13 +0530

    Create Game with nos.java

---
+++ Easy Level Program/Arrays/Game with nos.java
@@ -0,0 +1,9 @@
+class Complete{
+    public static int[] game_with_number (int arr[], int n) {
+        for(int d=0;d < n-1;d++)
+        {
+            arr[d] = arr[d]^arr[d+1];
+        }
+        return arr;
+    }
+}
