aefb59e1524b892a45b096ec5650354b67f829a7
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 18:43:22 +0530

    Create Palindrome String.java

---
+++ Easy Level Program/Strings/Palindrome String.java
@@ -0,0 +1,13 @@
+class Solution {
+    int isPalindrome(String S) {
+        // code here
+        for(int i = 0;i < S.length();i++)
+        {
+            if(S.charAt(i) != S.charAt(S.length()-i-1))
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
